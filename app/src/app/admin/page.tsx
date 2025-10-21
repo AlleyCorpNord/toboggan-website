317f7acbc1985f9873c20c861b5d2d7396ed7afa
Aboubacar OUATTARA <zie-aboubacar@alleycorpnord.com>
2025-10-21 16:53:39 -0400

    feat: init decap cms

---
+++ app/src/app/admin/page.tsx
@@ -0,0 +1,15 @@
+export const AdminPage = () => {
+  return <html>
+  <head>
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="robots" content="noindex" />
+    <title>Content Manager</title>
+  </head>
+  <body>
+    <script src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js"></script>
+  </body>
+</html>
+};
+
+export default AdminPage;
---
+++ app/src/app/layout.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return children;
+}
